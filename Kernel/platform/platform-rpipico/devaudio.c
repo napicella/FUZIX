4abe04a06980b25bda6590447b4a8cb47cb4060e
napicell <napicella4@gmail.com>
2024-07-21 13:06:59 +0200

    Call ioctl play for each sample. Solution is too slow

--- Kernel/platform/platform-rpipico/devaudio.c
+++ Kernel/platform/platform-rpipico/devaudio.c
@@ -1,8 +1,6 @@
 #include <kernel.h>
 #include <audio.h>
 #include <printf.h>
-#include "./audio/buzzer.h"
-#include "./audio/mario_tune.h"
 #include "picosdk.h"
 
 /* Simple two channel beeper style ton generator with no volume control */
@@ -12,89 +10,78 @@ const struct audio audio_info = {AUDIO_BEEPER, 2, 0};
 #define PMW_PIN 18
 #define HALT_BUTTON_PIN 20
 
-void play(ezBuzzer* buz, const int* melody, const int* noteDurations, int length) {
-  playMelody(buz, melody, noteDurations, length);
-  kprintf("after playMelody 7\n");
+static int count = 0;
+static bool playing = false;
 
-  while (true) {
-    loop(buz);
-
-    if (gpio_get(HALT_BUTTON_PIN) == 0) {
-      stop(buz);
-      break;
-    }
-    if (getState(buz) == BUZZER_IDLE) {
-      break;
-    }
+// d - the duty cycle (0 -> 100). For a square wave, select 50.
+uint32_t pwm_set_freq_duty(uint slice_num, uint chan, uint32_t f, int d) {
+  // Pi pico has a 12-bit fractional divider, where 12 bits can represent values
+  // from 0 to 4095 (2^12 = 4096). This gives a high resolution for dividing the
+  // base clock.
+  //
+  // The PWM signal period is determined by how many clock cycles fit into one
+  // period of the PWM signal. Without a divider, the period would be very short
+  // if the base clock frequency is high (e.g., 125 MHz). This short period
+  // limits the number of discrete steps that can be used within one PWM cycle.
+  uint32_t clock = 125000000;
+  uint32_t divider16 = clock / f / 4096 + (clock % (f * 4096) != 0);
+  if (divider16 / 16 == 0) {
+    divider16 = 16;
   }
+
+  uint32_t wrap = clock * 16 / divider16 / f - 1;
+
+  // divider16 / 16  - integer part
+  // divider16 & 0xF - fractional part (last 4 bits)
+  pwm_set_clkdiv_int_frac(slice_num, divider16 / 16, divider16 & 0xF);
+  pwm_set_wrap(slice_num, wrap);
+  pwm_set_chan_level(slice_num, chan, wrap * d / 100);
+
+  return wrap;
 }
 
+// Function to set the PWM frequency
+void tone(uint pin, uint32_t frequency, int duration) {
+  kprintf("tone pin %d and freq %d\n", pin, frequency);
+  uint slice_num = pwm_gpio_to_slice_num(pin);
+  pwm_set_freq_duty(slice_num, pwm_gpio_to_channel(pin), frequency, duration);
+  pwm_set_enabled(slice_num, true);
+}
 
+// Function to disable the PWM
+void noTone(uint pin) {
+  kprintf("no-tone\n");
+  uint slice_num = pwm_gpio_to_slice_num(pin);
+  pwm_set_enabled(slice_num, false);
+}
 
 // Interfaces that needs to be implemented
 
 int devaudio_play(void) {
-  kprintf("playing\n");
-
-  const uint LED_PIN = 6;
-  gpio_set_function(18, GPIO_FUNC_PWM);
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
-  gpio_init(HALT_BUTTON_PIN);
-  gpio_set_dir(HALT_BUTTON_PIN, GPIO_IN);
-  gpio_put(LED_PIN, 1);
-
-  ezBuzzer buz = {0};
-  buz._buzzerPin = GPIO_FUNC_PWM;
-  buz._buzzerState = BUZZER_IDLE;
-  buz._delayTime = 0;
-  buz._beepTime = 0;
-  buz._startTime = 0;
-  buz._melody = 0;
-  buz._noteDurations = 0;
-  buz._melodyLength = 0;
-  buz._melodyIndex = 0;
-  buz._notePauseTime = 0;
-
-  
-  int length = sizeof(smbIntroNotes) / sizeof(int);
-  play(&buz, smbIntroNotes, smbIntroDurations,length);
-
-  length = sizeof(smbANotes) / sizeof(int);
-  play(&buz, smbANotes, smbADurations, length);
-  
-  length = sizeof(smbBNotes) / sizeof(int);
-  play(&buz, smbBNotes, smbBDurations, length);
-  
-  // length = sizeof(smbCNotes) / sizeof(int);
-  // play(&buz, smbCNotes, smbCDurations, length);
-  
-  // length = sizeof(smbDNotes) / sizeof(int);
-  // play(&buz, smbDNotes, smbDDurations, length);
-
-  gpio_put(LED_PIN, 0);
+  tone(18, sound.freq, 50);
   return 0;
 }
 
 void devaudio_stop(uint8_t channel) {
-  //kprintf("stopped\n");
+  noTone(18);
 }
 
 int devaudio_wait(uint8_t channel) {
-  //kprintf("waiting\n");
-  return 0;
+  used(op);
+  used(val);
+  return -EINVAL;
 }
 
 
 int devaudio_ioctl(uarg_t op, void *val) {
-  //kprintf("ioctl\n");
-  // used(op);
-  // used(val);
-  // return -EINVAL;
-
+  gpio_set_function(18, GPIO_FUNC_PWM);
   return 0;
 }
 
 void devaudio_tick(void) {
-
+  // count++;
+  // if (sound.time > count) {
+  //   noTone(18);
+  //   count = 0;
+  // }
 }
